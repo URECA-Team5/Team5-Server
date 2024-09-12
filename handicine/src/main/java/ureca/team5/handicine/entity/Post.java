cee9f6b8fc3fd738f24883eaa4b795d91c401fe3
Geonhwi Park <ghpark715@gmail.com>
2024-09-12 14:45:49 +0900

    feat: 엔티티 추가
    - Answer, Comment, Post, Question, Role 엔티티 추가

---
+++ handicine/src/main/java/ureca/team5/handicine/entity/Post.java
@@ -0,0 +1,36 @@
+package ureca.team5.handicine.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Setter
+public class Post {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long postId;
+
+    private String title;
+    private String content;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+
+    @PrePersist
+    protected void onCreate() {
+        createdAt = LocalDateTime.now();
+    }
+
+    @PreUpdate
+    protected void onUpdate() {
+        updatedAt = LocalDateTime.now();
+    }
+}
---
+++ handicine/src/main/java/ureca/team5/handicine/entity/Role.java
@@ -0,0 +1,16 @@
+package ureca.team5.handicine.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class Role {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long roleId;
+
+    private String roleName;
+}
