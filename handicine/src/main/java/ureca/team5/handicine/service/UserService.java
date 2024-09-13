e923e13a6ab54b239b218f9826401303a11d0cb9
Geonhwi Park <geonhwipark715@gmail.com>
2024-09-13 09:31:23 +0900

    update: UserService
    login, logout 구현

--- handicine/src/main/java/ureca/team5/handicine/service/UserService.java
+++ handicine/src/main/java/ureca/team5/handicine/service/UserService.java
@@ -1,9 +1,11 @@
 package ureca.team5.handicine.service;
 
+import ureca.team5.handicine.dto.LoginRequestDTO;
 import ureca.team5.handicine.dto.UserDTO;
 import ureca.team5.handicine.entity.User;
 import ureca.team5.handicine.repository.UserRepository;
 import ureca.team5.handicine.repository.RoleRepository;
+import ureca.team5.handicine.security.JwtTokenProvider;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
@@ -21,6 +23,27 @@ public class UserService {
 
     @Autowired
     private PasswordEncoder passwordEncoder;
+
+    @Autowired
+    private JwtTokenProvider jwtTokenProvider;
+
+    public String login(LoginRequestDTO loginRequest) {
+        User user = userRepository.findByUsername(loginRequest.getUsername())
+                .orElseThrow(() -> new RuntimeException("User not found"));
+
+        if (!passwordEncoder.matches(loginRequest.getPassword(), user.getPassword())) {
+            throw new RuntimeException("Invalid credentials");
+        }
+
+        // JWT 토큰 생성
+        return jwtTokenProvider.createToken(user.getUsername(), user.getRole().getRoleName());
+    }
+
+    public void logout() {
+        // 클라이언트 측에서 JWT 토큰을 삭제하는 방식으로 로그아웃 처리
+        // 서버 측에서는 특별한 처리가 필요하지 않음 (서버는 JWT 상태를 유지하지 않기 때문)
+        // 토큰 무효화 기능이 필요할 경우 Redis 등을 활용하여 블랙리스트 방식으로 구현할 수 있음
+    }
 
     public UserDTO getUserById(Long id) {
         Optional<User> user = userRepository.findById(id);
