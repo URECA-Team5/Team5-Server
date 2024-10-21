e3ad5ad104d78d38b9c056a5e058d48d058fec11
Geonhwi Park <ghpark715@gmail.com>
2024-10-21 18:22:04 +0900

    Update: 불필요한 코드 제거

--- handicine/src/main/java/ureca/team5/handicine/controller/MedicineController.java
+++ handicine/src/main/java/ureca/team5/handicine/controller/MedicineController.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 import ureca.team5.handicine.entity.Medicine;
 import ureca.team5.handicine.service.MedicineService;
 //import org.springframework.web.bind.annotation.RestController;
-//PharmacyController.java
+
 @RestController
 @CrossOrigin(origins = "http://localhost:3000")
 @RequestMapping("/api/medicines")
